8915a6ee9925bcd3e7137daced327af6e3039f1f
VladDuncea <vlad_duncea_31@yahoo.com>
2019-02-28 20:57:07 +0200

    Implementation with classes

--- ImplementareAFN/main.cpp
+++ ImplementareAFN/main.cpp
@@ -1,129 +1,163 @@
 #include <iostream>
 #include <cstdlib>
+#include <string.h>
 
 using namespace std;
 
-struct state;
-
-struct tranzition
+class FiniteAutomaton
 {
-	struct state *next_state;
-	char letter;
-};
+	struct state;
 
-struct state
-{
-	int state_id;
-	//Maybe use vectors
-	tranzition *tranz;
-	int nr_tranz;
-	bool f_state;
-};
-
-void display_automaton(int n, state states[])
-{
-	for (int i = 0; i < n; i++)
+	struct tranzition
 	{
-		cout << "State: " << i << endl;
-		cout << "Final state: ";
-		if (states[i].f_state)
-			cout << "YES";
-		else
-			cout << "NO";
-		cout << endl;
+		struct state *next_state;
+		char letter;
+	};
 
-		for (int j = 0; j < states[i].nr_tranz; j++)
+	struct state
+	{
+		int state_id;
+		//Maybe use vectors
+		tranzition *tranz;
+		int nr_tranz;
+		bool f_state;
+	};
+
+	state * pstates, *initial;
+	int nr_states;
+
+	bool priv_check_word(state *cur_state, char word[], int wpoz)
+	{
+		int len;
+		len = strlen(word);
+		bool ok = false;
+
+		//Lambda word
+		if (wpoz == len)
 		{
-			cout << "On letter: " << states[i].tranz[j].letter << " go to " << states[i].tranz[j].next_state->state_id << endl;
+			if (cur_state->f_state)
+				return true;
+			return false;
 		}
-		cout << endl;
-	}
-}
 
-bool check_word(state *state,char word[],int wpoz)
-{
-	int len;
-	len = strlen(word);
-	bool ok=false;
-	
-	//Lambda word
-	if (wpoz == len)
-	{
-		if (state->f_state)
-			return true;
+		//Check if a tranzition is available
+		for (int i = 0; i < cur_state->nr_tranz; i++)
+		{
+			if (cur_state->tranz[i].letter == word[wpoz])
+			{
+				cur_state = cur_state->tranz[i].next_state;
+				ok = true;
+			}
+		}
+
+		if (ok)
+			return priv_check_word(cur_state, word, wpoz + 1);
+
 		return false;
 	}
 
-	//Check if a tranzition is available
-	for (int i = 0; i < state->nr_tranz; i++)
+public:
+	FiniteAutomaton()
 	{
-		if (state->tranz[i].letter == word[wpoz])
+		pstates = initial = NULL;
+	}
+
+	int initialize()
+	{
+		if (pstates != NULL)
 		{
-			state = state->tranz[i].next_state;
-			ok = true;
+			cout << "Machine already initialized!";
+			return -1;
+		}
+
+		cout << "Numar de stari: ";
+		cin >> nr_states;
+		pstates = new state[nr_states];
+		if (pstates == NULL)
+			return -1;
+		for (int i = 0; i < nr_states; i++)
+		{
+			pstates[i].state_id = i;
+			pstates[i].nr_tranz = 0;
+			pstates[i].tranz = NULL;
+			pstates[i].f_state = false;
+		}
+
+		//Initial state
+		int x;
+		cout << "Starea initiala (0-" << nr_states - 1 << "): ";
+		cin >> x;
+		initial = &pstates[x];
+
+		//Final states
+		cout << "Number of final states: ";
+		cin >> x;
+		for (int i = 0; i < x; i++)
+		{
+			int fs;
+			cin >> fs;
+			pstates[fs].f_state = true;
+		}
+
+		//Tranzitions
+		cout << "Numar tranzitii: ";
+		cin >> x;
+		cout << "Tranzitii 'stare 1' 'stare 2' 'litera'\n";
+
+		for (int i = 0; i < x; i++)
+		{
+			int s1, s2;
+			char l;
+			cin >> s1 >> s2 >> l;
+			int n = ++(pstates[s1].nr_tranz);
+			tranzition * aux = (tranzition *)realloc(pstates[s1].tranz, n);
+			if (aux == NULL)
+				return 0;
+			pstates[s1].tranz = aux;
+			pstates[s1].tranz[n - 1].letter = l;
+			pstates[s1].tranz[n - 1].next_state = &pstates[s2];
+		}
+		cout << "Initial state: " << initial->state_id << endl << endl;
+	}
+
+	void display_automaton()
+	{
+		if (pstates == NULL)
+		{
+			return;
+		}
+
+		for (int i = 0; i < nr_states; i++)
+		{
+			cout << "State: " << i << endl;
+			cout << "Final state: ";
+			if (pstates[i].f_state)
+				cout << "YES";
+			else
+				cout << "NO";
+			cout << endl;
+
+			for (int j = 0; j < pstates[i].nr_tranz; j++)
+			{
+				cout << "On letter: " << pstates[i].tranz[j].letter << " go to " << pstates[i].tranz[j].next_state->state_id << endl;
+			}
+			cout << endl;
 		}
 	}
-			
-	if(ok)
-		return check_word(state,word,wpoz+1);
 
-	return false;
-}
+	bool check_word(char word[])
+	{
+		return priv_check_word(initial, word, 0);
+	}
+};
+
 
 int main()
 {
-	state * pstates, *initial;
-	int n;
-	//the number of states and initialization
-	cout << "Numar de stari: ";
-	cin >> n;
-	pstates = new state[n];
-	if (pstates == NULL)
-		return -1;
-	for (int i = 0; i < n; i++)
-	{
-		pstates[i].state_id = i;
-		pstates[i].nr_tranz = 0;
-		pstates[i].tranz = NULL;
-		pstates[i].f_state = false;
-	}
+	FiniteAutomaton fa;
+	fa.initialize();
 
-	//Initial state
-	int x;
-	cout << "Starea initiala (0-" << n - 1 << "): ";
-	cin >> x;
-	initial = &pstates[x];
-	//Final states
-	cout << "Number of final states: ";
-	cin >> x;
-	for (int i = 0; i < x; i++)
-	{
-		int fs;
-		cin >> fs;
-		pstates[i].f_state = true;
-	}
-
-	//Tranzitions
-	cout << "Numar tranzitii: ";
-	cin >> x;
-	cout << "Tranzitii 'stare 1' 'stare 2' 'litera'\n";
-
-	for (int i = 0; i < x; i++)
-	{
-		int s1, s2;
-		char l;
-		cin >> s1 >> s2 >> l;
-		int n = ++(pstates[s1].nr_tranz);
-		tranzition * aux = (tranzition *)realloc(pstates[s1].tranz, n);
-		if (aux == NULL)
-			return 0;
-		pstates[s1].tranz = aux;
-		pstates[s1].tranz[n - 1].letter = l;
-		pstates[s1].tranz[n - 1].next_state = &pstates[s2];
-	}
-	cout << "Initial state: " << initial->state_id << endl<<endl;
-	display_automaton(n, pstates);
-
+	fa.display_automaton();
 	//Read words
 	while (true)
 	{
@@ -136,9 +170,9 @@ int main()
 
 		cout << "Insert word: ";
 		cin >> word;
-		cout << check_word(initial, word, 0);
+		cout << "The word : " << word << " is " << (fa.check_word(word) ? "correct" : "incorrect" ) << endl;
 	}
-	
+
 
 	return 0;
 }
